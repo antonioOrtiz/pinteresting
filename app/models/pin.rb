2118abd91fc87f95ffab1d114f43998f9403fd23
antonio ortiz <17antonio.ortiz@gmail.com>
2015-07-18 08:54:22 -0400

    Add Amazon S3 for Paperclip uploads to Heroku, take 5

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,5 @@
-Class Pin < ActiveRecord::Base
-    has_attached_file :image,
-            :styles => { :medium => "x300", :thumb => "x100" },
-            :storage => :s3,
-            :bucket => "my-pinteresting-app",
-            :s3_credentials => {:access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}
+class Pin < ActiveRecord::Base
+	belongs_to :user
+
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
