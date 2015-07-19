16fcc44f4eda231e6714a017d4c5930aade65b73
antonio ortiz <17antonio.ortiz@gmail.com>
2015-07-19 10:53:23 -0400

    Added SendGrid configurations to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['app25957121@heroku.com'],
-  :password       => ENV['ufl5vt544811'],
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
@@ -22,8 +22,8 @@ Mail.defaults do
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :domain => 'heroku.com',
-    :user_name => ENV['app25957121@heroku.com'],
-    :password => ENV['ufl5vt544811'],
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :authentication => :plain,
     :enable_starttls_auto => true
   }
